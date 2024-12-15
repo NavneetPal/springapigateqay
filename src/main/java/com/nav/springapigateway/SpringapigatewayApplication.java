109ad0555b4b68f63723e853cb761a51cf70d393
Navneet Pal <navneetpal8106@gmail.com>
2024-12-15 18:17:06 +0530

    added ratelimiter

--- src/main/java/com/nav/springapigateway/SpringapigatewayApplication.java
+++ src/main/java/com/nav/springapigateway/SpringapigatewayApplication.java
@@ -2,6 +2,9 @@ package com.nav.springapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 
 @SpringBootApplication
 public class SpringapigatewayApplication {
@@ -10,4 +13,8 @@ public class SpringapigatewayApplication {
 		SpringApplication.run(SpringapigatewayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver keyResolver(){
+		return exchange -> Mono.just("userKey");
+	}
 }
